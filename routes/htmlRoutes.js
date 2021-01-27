21316053e3d217b84236faa8dd2e4adb5c898230
Linh Nguyen <linhnhatmen@gmail.com>
2021-01-27 10:51:35 +0700

    html routes handling rendering the pages

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+// import modules
+const router = require("express").Router();
+const path = require("path");
+
+// index page
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+//exercise page
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+// stats page
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
