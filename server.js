03665ec5d3518e2b4b5e34bfcd77af6a208aacf3
Linh Nguyen <linhnhatmen@gmail.com>
2021-01-27 10:52:23 +0700

    app use routes

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
+
 //Setup connection to mongoose DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
